b50ddf9e67c5b7ce0cac06a6c928c845362da0a2
gatsbimantico <gatsbimantico@gmail.com>
2017-05-24 02:30:32 +0100

    CV Update

--- cv.js
+++ cv.js
@@ -6,43 +6,38 @@
 
 var Cesar = new Candidate({
 	name: 'César Costas Carrera',
-	role: 'Mid-Level JavaScript Engineer',
-	skills: [
-		'FrontEnd enthusiast',
-		'Fast learner',
-		'Team builder',
-		'Agile developer'
-	],
+	role: 'JavaScript Engineer',
 	contact: {
 		phone: '+447920158374',
-		emails: [
-			'costascarrera.cesar@gmail.com',
-			'costascarrera.cesar@outlook.com'
-		],
+		email: 'costascarrera.cesar@gmail.com',
 		sites: [
 			'//github.com/gatsbimantico',
 			'//stackoverflow.com/users/2308745/gatsbimantico'
 		]
 	},
 	summary: `
-		On 2010 I was finishing my degree while doing a work experience in a chemistry lab.
+I am a front end web developer with over 3 years of experience.
 
-		I was introduced to data mining (neural networks),
-		an experience that will lead me to work as data analyst in a different lab.
+My passion for front end development begun while working on web data visualization as a data analyst. Since then I have worked on projects for a number of clients including Volkswagen, Lipton and Inditex Group.
 
-		During this job, I felt in love with data visualization and front-end web development.
+I'm fully competent with modern frameworks (Polymer or Angular), frontend utilities (NodeJs, Gulp, Git, Docker) and agile methodologies (Scrum or Khanban).
 
-		Thus I've been working as web developer since then.
-	`
+	`,
+	skills: [
+		'FrontEnd enthusiast',
+		'Fast learner',
+		'Team builder',
+		'Agile developer'
+	]
 });
 
 Cesar.addExperience({
 
 	is: 'FRONT END WEB DEVELOPER',
 	at: 'TRIBAL WORLDWIDE',
-	on: { start: '2017 JAN', end: undefined },
+	on: { start: '2017 JAN', end: 'present' },
 	tools: {
-		front: ['POLYMER', 'JQUERY', 'GULP', 'GIT', 'Handlebars', 'WordPress', 'AEM'],
+		front: ['POLYMER', 'JQUERY', 'GULP', 'GIT', 'HANDELBARS', 'WORDPRESS', 'AEM'],
 	},
 	portfolio: {
 		released: [
@@ -54,16 +49,15 @@ Cesar.addExperience({
 		]
 	},
 	description: `
-		I'm currently working at Tribal.
-		This is an hybrid Ad-Development agency that provides websites to their advertising clients.
-		On each project we are a small team of 2 to 4 specialists.
+		Tribal is an hybrid Ad-Development agency that provides websites to their advertising clients.
+		Each project is handled by a small team of 2 to 4 specialists.
 
-		We use gulp and git for the front end, then the backend supplies the data.
-		Depending on the project I used Polymer, jQuery and Handlebars, or pure HTML and CSS.
+		We use gulp and git for the front end, then the backend supplies the data. 
+		I managed front-end development using my expertise in Polymer, jQuery and Handlebars, or pure HTML and CSS.
 		Also had to work with AEM and Jahia.
 
 		The workflow is a ticket based board with JIRA and Scrum.
-		The goal is to provide the maximum value within the stimated time for the ticket.
+		The goal is to provide the maximum value within the estimated time for the ticket.
 		The time management is done in JIRA and Projector PSA.
 	`
 
@@ -79,19 +73,15 @@ Cesar.addExperience({
 		back: []
 	},
 	description: `
-		While I was working at Imatia,
-		I was working on the design and implementation
-		of a business analytics dashboard for local administrations.
+		At Imatia I worked on the design and implementation of a business analytics dashboard for local administrations.
 		
-		We were a small team of 4 developers
-		and my job goal was to define and implement the data workflow and its visualization.
+		Working with a team of 4 developers, I defined and implemented the data workflow and its visualization.
 
-		We used Docker and Jekins for automatic deploy,
-		Java to serve REST decoupled services
+		I used Docker and Jekins for automatic deploy, Java to serve REST decoupled services 
 		and Angular2 with TypeScript for the web components based front-end development.
 
 		The workflow was to acomplish the tasks written on Redmine by the project manager,
-		commit the work every time it reaches an stable status
+		commit the work every time it reaches a stable status
 		and to keep documented the time invested in every task through Elastic Projects.
 	`
 
@@ -119,14 +109,13 @@ Cesar.addExperience({
 		]
 	},
 	description: `
-		During this period I worked in web maintenance
-		for some international Inditex's websites
-		(MassimoDutti, ZaraHome, Oysho, Bershka, Stradivarius and PullAndBear).
+		I managed web maintenance for a range of international Inditex's websites including 
+		MassimoDutti, ZaraHome, Oysho, Bershka, Stradivarius and PullAndBear.
 
-		We were a medium size team of about 20 developers, 15 testers and 10 project managers,
-		with people working on remote.
+		I worked within a team of about 20 developers, 15 testers and 10 project managers,
+		with some remote team memebers.
 
-		We used IBM tools for the backend and jquery for the frontend.
+		I used IBM tools for the backend and jquery for the frontend.
 		While I was working there, we were migrating the web
 		to use a decoupled backend via REST and migrating to Angular1.4
 
@@ -173,7 +162,7 @@ Cesar.addEducation([{
 	titles: [
 		'Time Management',
 		'How to speak in public'
-	], at: 'COURSERA', on: 2017
+	], at: 'TRIBAL', on: 2017
 
 },{
 
